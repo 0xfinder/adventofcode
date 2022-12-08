89fc48da64127c0b7e7b41878d058973f8f9d319
0xfinder <0xfinder@protonmail.com>
2022-12-08 20:25:21 +0800

    day 2

---
+++ 2022/day1b/target/debug/deps/day1b-93aee45c3c9a56fe.d
@@ -0,0 +1,5 @@
+d:\Code\rust\adventofcode\2022\day1b\target\debug\deps\day1b-93aee45c3c9a56fe.rmeta: src\main.rs
+
+d:\Code\rust\adventofcode\2022\day1b\target\debug\deps\day1b-93aee45c3c9a56fe.d: src\main.rs
+
+src\main.rs:
---
+++ 2022/day2b/src/main.rs
@@ -0,0 +1,63 @@
+use std::fs;
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let file = fs::File::open("input.txt").expect("File not found");
+    let reader = io::BufReader::new(file);
+
+    let mut score = 0;
+
+    // A for Rock, B for Paper, and C for Scissors
+    // 1 2 3
+    // X Y Z
+    // lose draw win
+    let mut shape = "";
+
+    for line in reader.lines() {
+        let line = line.expect("Error reading line");
+
+        let (opponent, me) = line.split_once(" ").unwrap();
+
+        let outcome_score = match (opponent, me) {
+            ("A", "X") | ("B", "X") | ("C", "X") => {
+                shape = match opponent {
+                    "A" => "Z",
+                    "B" => "X",
+                    "C" => "Y",
+                    _ => "",
+                };
+                0
+            }
+            ("A", "Y") | ("B", "Y") | ("C", "Y") => {
+                shape = match opponent {
+                    "A" => "X",
+                    "B" => "Y",
+                    "C" => "Z",
+                    _ => "",
+                };
+                3
+            }
+            ("A", "Z") | ("B", "Z") | ("C", "Z") => {
+                shape = match opponent {
+                    "A" => "Y",
+                    "B" => "Z",
+                    "C" => "X",
+                    _ => "",
+                };
+                6
+            }
+            _ => 0,
+        };
+
+        let shape_score = match shape {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => 0,
+        };
+
+        score += outcome_score + shape_score;
+    }
+    println!("{:?}", score);
+}
